41af6f53d6a9ea20f40fea501314aecc797a6aca
Vinicius Xavier <viniciusxavier45@hotmail.com>
2023-08-29 15:42:43 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		
+	System.out.println("Hello world!");
+	System.out.println("teste");
+		
+		
+	}
+
+}
