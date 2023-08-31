901deb047b3bed67b2c9412dddd7de896cc5ce83
Vinicius Xavier <viniciusxavier45@hotmail.com>
2023-08-31 17:25:50 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -46,6 +46,23 @@ public class Board {
 		piece.position = position;			
 	}
 	
+	public Piece removePiece (Position position) {
+		if(!positionExist(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
+	
+	
+	
+	
 	public boolean positionExist(int row, int column) {
 		return row >=0 && row <rows && column >=0 && column < columns;
 	}
